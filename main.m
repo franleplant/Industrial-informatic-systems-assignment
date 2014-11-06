eae6927aabf9d18f1f4c406f501ee64d58a795b3
franleplant <franleplant@gmail.com>
2014-11-06 20:25:50 -0300

    remove c point

--- main.m
+++ main.m
@@ -17,7 +17,7 @@ X0 = [823 ; 1000];
 
 
 % Parameters
-X_MAX_RANGE = 18;
+X_MAX_RANGE = 20;
 range = 0:X_MAX_RANGE;
 I = eye(2);
 dt = 0.01;
@@ -48,46 +48,5 @@ p1 = plot(t_axis, Points(1, 1)); hold on;
 p2 = plot(t_axis, Points(2, 1));
 set(p1, 'Color', 'blue');
 set(p2, 'Color', 'red');
-
-
-
-%
-% c)
-%
-
-if 0 
-B = [2.193 0; 0 -0.05];
-u = [250; 2000];
-
-syms tau;
-
-O1_tau = subs(O1, t, t - tau);
-
-X1 = simplify( O1*X0 + int( O1_tau*B*u, tau, 0, t ) )
-
-
-subplot(2,2,3);
-p1 = ezplot(X1(1, 1), range); hold on;
-p2 = ezplot(X1(2, 1), range);
-set(p1, 'Color', 'blue');
-set(p2, 'Color', 'red');
-
-
-
-O2_tau = subs(O2, t, t - tau);
-
-X2 = simplify( O2*X0 + int( O2_tau*B*u, tau, 0, t ) )
-
-
-subplot(2,2,4);
-p1 = ezplot(X2(1, 1), range); hold on;
-p2 = ezplot(X2(2, 1), range);
-set(p1, 'Color', 'blue');
-set(p2, 'Color', 'red');
-
-
-
-end
-
 
 
