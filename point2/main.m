74dfcc804e54d3c899ada191182fdd52431dc821
franleplant <franleplant@gmail.com>
2014-11-18 20:56:21 -0300

    progress on point 2

--- point2/main.m
+++ point2/main.m
@@ -8,3 +8,16 @@ G5 = tf([1.28], [.0033 1]);
 G6 = tf([20], [.03 1]);
 G7 = tf([1], [.44 0]);
 H1 = tf([1], [.005 1]);
+
+
+M1 = G1*A1*G2*G3*A2*G4*G5*G6*G7;
+L1 = (A1*G2*G3*A2*G4*G5*G6*G7*(-1)*H1) + (A2*G4*G5*G6*(-1)) + (G6*G7*(-1));
+
+M = M1/L1
+
+
+bode(M);
+
+
+% To get the array representation of M
+% [num, den] = tfdata(M, 'v')
